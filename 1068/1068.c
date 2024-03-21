32ad14182e4e04cef9b5f64ab786bcfad542654f
zegiha <LeeSeoYul070814@gmail.com>
2024-03-21 21:29:17 +0900

    solving 1068

--- 1068/1068.c
+++ 1068/1068.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int n, node[51][51], del, leafnode;
+int N, arr[51], del;
+typedef struct stackType {
+    int e[51];
+    int top;
+} stackType;
 
-void getLeafnode(int i) {
-    int sw = 1;
-    for(int j = i + 1; j < n; j++) {
-        if(node[i][j] && j != del) {
-            sw = 0;
-            getLeafnode(j);
+void push(stackType* stack, int element) {
+    stack -> e[++stack -> top] = element;
+}
+
+void getLeafnode(int i, stackType stack) {
+    for(int index = i + 1; index < N; index++) {
+        if(arr[index] == i && index != del) {
+            push(&stack, index);
         }
     }
-    if(sw) leafnode++;
+
 }
 
 int main() {
-    scanf("%d", &n);
-    int tmpArr[51];
-    for(int i = 0; i < n; i++) scanf("%d", &tmpArr[i]);
-    for(int i = 0; i < n; i++) {
-        if(tmpArr[i] != -1) node[tmpArr[i]][i]++; 
-    }
+    scnaf("%d", &N);
+    for(int i = 0; i < N; i++) scanf("%d", &arr[i]);
     scanf("%d", &del);
 
-    if(del != 0) getLeafnode(0);
-    printf("%d", leafnode);
+    stackType stack;
+    stack.top = -1;
+    getLeafnode(0, stack);
 }
