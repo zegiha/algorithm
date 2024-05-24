6d9396579906c0978c3e6fca058f4246646f3be3
zegiha <LeeSeoYul070814@gmail.com>
2024-05-24 19:32:49 +0900

    solve 10942

--- ETC/test/text.cpp
+++ ETC/test/text.cpp
@@ -11,8 +11,8 @@ int main() {
     }
     for(int i = 1; i <= 7; i++) {
         for(int j = 1; j <= 7; j++) {
-            if(arr[i][j] == arr[i][0] * 2) cout << "● ";
-            else cout << "○ ";
+            if(arr[i][j] == arr[i][0] * 2) cout << "1 ";
+            else cout << "0 ";
         }
         cout << endl;
     }
