938c88f4739ca56b4d4c6ae370eccdf016f3354f
zegiha <leeseoyul070814@gmail.com>
2024-09-02 04:32:21 +0900

    solve 2470

--- gold/2470/2470.cpp
+++ gold/2470/2470.cpp
@@ -4,57 +4,61 @@
 using namespace std;
 
 struct ANS {
-  int v;
-  int f;
-  int s;
+  int value;
+  int i;
+  int ii;
+};
+struct DISTENCE {
+  int distence;
+  int index;
 };
 
-int n, arr[100000];
 
-int getSmallNearest(int pivot) {
-  // bool sw = pivot == -999999996 ? true : false;
-  int l = 0, r = n-1, m, ans = -2'100'000'000;
-  
+int n, arr[100'000];
+
+DISTENCE getUpperDistence(int pivot, int pivotIndex) {
+  int l = 0, r = n - 1, m, distence = 2'100'000'000;
+  DISTENCE ans = {2'100'000'000, -1};
+
   while(l <= r) {
     m = (l + r) / 2;
+    distence = abs(pivot - arr[m]);
 
-    if(arr[m] > pivot) {
-      // if(sw) cout << "haha1\n";
-      r= m -1;
-    } else if(arr[m] < pivot){
-      // if(sw) cout << "ha2\n";
-      ans = max(ans, arr[m]);
-      l = m +1;
-    } else return arr[m];
+    if(pivot <= arr[m]) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
 
-    if(l == r) ans = arr[m];
+    if(distence < ans.distence && m != pivotIndex) {
+      ans = {distence, m};
+    }
   }
 
   return ans;
 }
 
-int getBigNearest(int pivot) {
-  int l = 0, r = n - 1, m, ans = 2'100'000'000;
-  
+DISTENCE getLowerDistence(int pivot, int pivotIndex) {
+  int l = 0, r = n - 1, m, distence = 2'100'000'000;
+  DISTENCE ans = {2'100'000'000, -1};
+
   while(l <= r) {
     m = (l + r) / 2;
+    distence = abs(pivot - arr[m]);
 
-    if(arr[m] > pivot) {
-      r = m-1;
-      ans = min(ans, arr[m]);
-    } else if(arr[m] < pivot){
-      l = m+1;
-    } else return arr[m];
+    if(pivot >= arr[m]) {
+      l = m + 1;
+      
+    } else {
+      r = m - 1;
+    }
 
-    if(l == r) ans = arr[m];
+    if(distence < ans.distence && m != pivotIndex) {
+      ans = {distence, m};
+    }
   }
 
   return ans;
-}
-
-int getAbs(int req) {
-  if(req < 0) return req * -1;
-  return req;
 }
 
 int main() {
@@ -64,46 +68,28 @@ int main() {
   }
   sort(arr, arr + n);
 
-  ANS ans = {2'100'000'000, -1, -1};
+  ANS ans = {2'100'000'000, -1, -1}, newAns = {2'100'000'000, -1, -1};
   for(int i = 0; i < n; i++) {
-    int
-    smallNearest = getSmallNearest(arr[i] * -1),
-    bigNearest = getBigNearest(arr[i] * -1);
-    
-    if(smallNearest == arr[i] || smallNearest == -2'100'000'000) {
-      if(i - 1 > -1) {smallNearest = arr[i - 1];}
-    }
-    if(bigNearest == arr[i] || bigNearest == 2'100'000'000) {
-      if(i + 1 < n) {bigNearest = arr[i + 1];}
-    }
+    DISTENCE lowerDistence = getLowerDistence(arr[i] * -1, i);
+    DISTENCE upperDistence = getUpperDistence(arr[i] * -1, i);
 
-    ANS newAns = {2'100'000'000, -1, -1};
-    if(bigNearest == smallNearest && smallNearest == 2'100'000'000) continue;
-    
-    if(getAbs(arr[i] + smallNearest) > getAbs(arr[i] + bigNearest)) {
-      newAns = {getAbs(arr[i] + bigNearest), arr[i], bigNearest};
+    // cout << lowerDistence.index <<  ' ' << upperDistence.index << '\n';
+    if(lowerDistence.index == -1 && lowerDistence.index == upperDistence.index) continue;
+
+    if(lowerDistence.distence > upperDistence.distence) {
+      newAns = {abs(arr[i] + arr[upperDistence.index]), upperDistence.index, i};
     } else {
-      newAns = {getAbs(arr[i] + smallNearest), arr[i], smallNearest};
+      newAns = {abs(arr[i] + arr[lowerDistence.index]), lowerDistence.index, i};
     }
 
-    if(ans.v > newAns.v) {
-      if(newAns.f > newAns.s) {
-        int t = newAns.f;
-        newAns.f = newAns.s;
-        newAns.s = t;
-      }
-
-      ans = newAns;
-    }
+    if(ans.value > newAns.value) ans = newAns;
   }
 
-  cout << ans.f << ' ' << ans.s;
+  if(ans.i > ans.ii) {
+    int t = ans.i;
+    ans.i = ans.ii;
+    ans.ii = t;
+  }
+
+  cout << arr[ans.i] << ' ' << arr[ans.ii];
 }
-/*
-5
-1 1 1 1 1
-
-3
--10 1 2
-
-*/
