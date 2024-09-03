63fc60ebf54c67e476c1f503aea6dc4bf33c844e
zegiha <leeseoyul070814@gmail.com>
2024-09-03 10:20:12 +0900

    solve 4779

--- gold/2447/2447.cpp
+++ gold/2447/2447.cpp
@@ -1,16 +1,1 @@
-// #include <iostream>
-
-// using namespace std;
-
-// vector <string>
-
-// void backtrack(int l) {
-//   if(l == 3) {
-    
-//   }
-// }
-
-// int main() {
-//   int n; cin >> n;
-//   backtrack(n);
-// }
+// 포기
