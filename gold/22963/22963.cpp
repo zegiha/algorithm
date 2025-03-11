d3a0257e95662175f916d47f77655ebf340491a4
zegiha <leeseoyul070814@gmail.com>
2025-03-11 10:35:32 +0900

    solve 22963

--- gold/22963/22963.cpp
+++ gold/22963/22963.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include <vector>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define INF 2'100'000'000
 
 using namespace std;
 
-int n, arr[200'001];
-vector <pair <int, int>> ans;
+int n, dp[200'001][4], a[200'001];
 
 int main() {
+  fastio;
   cin >> n;
-  for(int i = 0; i < n; i++) {
-    cin >> arr[i];
-  }
-  for(int i = 0; i < n - 1; i++) {
-    if(arr[i] > arr[i + 1]) {
-      if(ans.size() >= 3) {
-        cout << "NO";
-        return 0;
-      } else {
-        arr[i + 1] = arr[i];
-        ans.push_back({i + 1, arr[i]});
-      }
+  for(int i = 1; i <= n; i++) cin >> a[i];
+
+  dp[1][0] = a[1]; dp[1][1] = dp[1][2] = dp[1][3] = 1;
+  for(int i = 2; i <= n; i++) {
+    dp[i][0] = dp[i-1][0] <= a[i] ? a[i] : INF;
+    for(int j = 1; j <= 3; j++) {
+      dp[i][j] = dp[i-1][j-1];
+      if(dp[i-1][j] <= a[i]) dp[i][j] = min(dp[i][j], a[i]);
     }
   }
-  
-  cout << "YES\n" << ans.size() << '\n';
-  for(int i = 0; i < ans.size(); i++) {
-    cout << ans[i].first << ' ' << ans[i].second << '\n';
-  }
+
+  int m = INF, idx = 0;
+  for(int i = 0; i < 4; i++) if(m > dp[n][i]) m = dp[n][i], idx = i;
+  if(m != INF) {
+    cout << "YES\n" << idx << "\n";
+    for(int i = n, j = idx; i >= 1 && j; i--) {
+      if(dp[i-1][j] > a[i] || dp[i-1][j-1] < a[i]) {
+        cout << i << ' ' << dp[i][j--] << '\n';
+      }
+    }
+  } else cout << "NO";
 }
