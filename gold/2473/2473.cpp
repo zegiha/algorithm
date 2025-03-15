c44b167167e552b63ac40ee19c0112be478f4454
zegiha <leeseoyul070814@gmail.com>
2025-03-15 09:26:05 +0900

    solving 2473

--- gold/2473/2473.cpp
+++ gold/2473/2473.cpp
@@ -1,40 +1,72 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
 
-#define INF 4'100'000'000
+// #define INF 4'200'000'000
 
-using namespace std;
-using ll = long long;
+// using namespace std;
+// using ll = long long;
 
-ll n, arr[5001];
-pair <ll, vector <int>> ans = {INF, vector <int> (0)};
+// int n;
+// ll arr[5001];
+// pair <ll, vector <int>> ans = {INF, vector <int> (0)};
 
-int main() {
-    cin >> n;
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr, arr + n);
+// int main() {
+//     cin >> n;
+//     for(int i = 0; i < n; i++) cin >> arr[i];
+//     sort(arr, arr + n);
 
-    for(int i = 0; i < n; i++) {
-        int l = 0, r = n-1;
-        ll sum;
-        while(l < r) {
-            sum = abs(arr[i] + arr[l] + arr[r]);
-            ll
-                l_sum = abs(arr[i] + arr[l + 1] + arr[r]),
-                r_sum = abs(arr[i] + arr[l] + arr[r-1]);
-            if(r-1 == i) r_sum = INF;
-            if(l+1 == i) l_sum = INF;
-            if(sum > min(l_sum, r_sum)) {
-                if(l_sum > r_sum) r--;
-                else l++;
-            } else break;
-        }
-        if(ans.first > sum) {
-            ans = {sum, vector <int> (0)};
-            ans.second.push_back(i); ans.second.push_back(l); ans.second.push_back(r);
-        }
-    }
+//     for(int i = 1; i < n-1; i++) {
+//         int l = 0, r = n-1;
+//         while(l < r) {
+            
+//         }
+//     }
+// }
 
-    for(int i = 0; i < 3; i++) cout << arr[ans.second[i]] << ' ';
-}
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+
+// #define INF 4'200'000'000
+
+// using namespace std;
+// using ll = long long;
+
+// int n;
+// ll arr[5001];
+// pair <ll, vector <int>> ans = {INF, vector <int> (0)};
+
+// int get_nearby(ll target) {
+//     int l = 0, r = n-1, m;
+
+//     while(l <= r) {
+//         m = (l + r) / 2;
+//         if(arr[m] < target) {
+//             l = m+1;
+//         } else {
+//             r = m-1;
+//         }
+//     }
+
+//     return l;
+// }
+
+// int main() {
+//     cin >> n;
+//     for(int i = 0; i < n; i++) cin >> arr[i];
+//     sort(arr, arr + n);
+
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < n; j++) {
+//             if(i == j) continue;
+//             int k = get_nearby((arr[i] + arr[j]) * -1);
+//             if(ans.first > abs(arr[k] + arr[i] + arr[j])) {
+//                 ans = {abs(arr[k] + arr[i] + arr[j]), vector <int> (0)};
+//                 ans.second.push_back(i); ans.second.push_back(j); ans.second.push_back(k);
+//             }
+//         }
+//     }
+
+//     for(int i = 0; i < 3; i++) cout << ans.second[i] << ' ';
+// }
