5d99c018c8449dfff83afaa2f7a8cc9b64dea060
zegiha <leeseoyul070814@gmail.com>
2025-03-19 11:46:43 +0900

    solve 25341

--- gold/25341/25341.cpp
+++ gold/25341/25341.cpp
@@ -12,28 +12,29 @@ struct node {
 };
 
 
-ll n, m, q;
+ll n, m, q, compressed_bias = 0;
 node out;
 vector <vector <ll>> input;
 vector <node> hidden;
+vector <ll> compressed_weight;
 
-void solve(int idx) {
-    ll ans = out.bias;
-    vector <ll> calc_hidden;
-    for(int i = 0; i < m; i++) {
-        ll sum = hidden[i].bias;
-        for(int j = 0; j < hidden[i].length; j++) {
-            sum += input[idx][hidden[i].input_idx[j]] * hidden[i].weight[j];
-        }
-        calc_hidden.push_back(sum);
-    }
+// void solve(int idx) {
+//     ll ans = out.bias;
+//     vector <ll> calc_hidden;
+//     for(int i = 0; i < m; i++) {
+//         ll sum = hidden[i].bias;
+//         for(int j = 0; j < hidden[i].length; j++) {
+//             sum += input[idx][hidden[i].input_idx[j]] * hidden[i].weight[j];
+//         }
+//         calc_hidden.push_back(sum);
+//     }
 
-    for(int i = 0; i < m; i++) {
-        ans += calc_hidden[out.input_idx[i]] * out.weight[i];
-    }
+//     for(int i = 0; i < m; i++) {
+//         ans += calc_hidden[out.input_idx[i]] * out.weight[i];
+//     }
 
-    cout << ans << '\n';
-}
+//     cout << ans << '\n';
+// }
 
 int main() {
     cin >> n >> m >> q;
@@ -69,14 +70,23 @@ int main() {
 
     // cout << "after out\n";
 
+    compressed_bias = out.bias;
+    compressed_weight.resize(n);
+    for(int i = 0; i < m; i++) {
+        compressed_bias += hidden[i].bias * out.weight[i];
+        for(int j = 0; j < hidden[i].length; j++) {
+            compressed_weight[hidden[i].input_idx[j]] += out.weight[i] * hidden[i].weight[j];
+        }
+    }
+
     for(int i = 0; i < q; i++) {
-        input.push_back(vector <ll> (n));
-        for(int j = 0; j < n; j++) cin >> input[i][j];
+        ll ans = compressed_bias;
+        for(int j = 0; j < n; j++) {
+            int t; cin >> t;
+            ans += compressed_weight[j] * t;
+        }
+        cout << ans << '\n';
     }
 
     // cout << "after input\n";
-
-    for(int i = 0; i < q; i++) {
-        solve(i);
-    }
 }
