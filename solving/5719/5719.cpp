22c466aace8453a3dd8b1bdaeea99d60ea4c304f
zegiha <leeseoyul070814@gmail.com>
2025-04-10 09:36:52 +0900

    solving 5719

--- solving/5719/5719.cpp
+++ solving/5719/5719.cpp
@@ -13,6 +13,8 @@ vvpii arr;
 void input() {
   int u, v, p;
   arr.resize(n, vector <pii> (0));
+
+  cin >> s >> d;
   for(int i = 0; i < m; i++) {
     cin >> u >> v >> p;
     arr[u].push_back({p, v});
@@ -22,7 +24,6 @@ void input() {
 int getMinDist() {
   priority_queue <pii, vector <pii>, greater<pii>> q;
   vector <int> dist(n, -1);
-  int res = INT_MAX;
 
   dist[s] = 0;
   q.push({0, s});
@@ -34,6 +35,7 @@ int getMinDist() {
     if(dist[node] != -1 && dist[node] < value) continue;
 
     for(int i = 0; i < arr[node].size(); i++) {
+      if(arr[node][i].second == -1) continue;
       int newNode = arr[node][i].second, newValue = value + arr[node][i].first;
       if(dist[newNode] == -1 || newValue < dist[newNode]) {
         dist[newNode] = newValue;
@@ -45,35 +47,28 @@ int getMinDist() {
   return dist[d];
 }
 
-vector <bool> visited(n, false);
-void setAlmostMinDist(int minDist, int node, int value) {
-  if(node == d && minDist < value) ans = min(value, ans);
-  for(int i = 0; i < arr[node].size(); i++) {
-    if(!visited[arr[node][i].second]) {
-      visited[arr[node][i].second] = true;
-      setAlmostMinDist(minDist, arr[node][i].second, value + arr[node][i].first);
-      visited[arr[node][i].second] = false;
-    }
-  }
+void deleteMinDist(int minDist) {
+  
 }
 
 void solve() {
-  setAlmostMinDist(getMinDist(), s, 0);
-  if(ans != INT_MAX) cout << ans << '\n';
+  int minDist = getMinDist();
+  deleteMinDist(minDist);
+  ans = getMinDist();
+  cout << "=========";
+  if(ans != minDist) cout << ans << '\n';
   else cout << -1 << '\n';
 }
 
 int main() {
-  cin >> n >> m;
   while(true) {
-    cin >> s >> d;
-    if(s == 0 && d == 0) break;
+    cin >> n >> m;
+    if(n == 0 && m == 0) break;
 
     input();
     solve();
     
     arr.clear();
-    visited.clear();
     ans = INT_MAX;
   }
 }
