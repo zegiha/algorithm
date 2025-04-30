e3c4508d872fa2a61881ef87281e41f85cf545d5
zegiha <leeseoyul070814@gmail.com>
2025-04-30 17:25:38 +0900

    solve 5670

--- platinum/5670/5670.cpp
+++ platinum/5670/5670.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 #include <stdio.h>
+#include <cmath>
 
 using namespace std;
 
@@ -9,8 +10,8 @@ int n;
 vector <string> arr;
 
 struct Trie {
-  int childrenCnt = 0;
   bool finish = false;
+  int childrenCnt = 0, finishCnt = 0;
   Trie* arr[26] = {};
 
   ~Trie() {
@@ -18,22 +19,37 @@ struct Trie {
       if(arr[i]) delete arr[i];
   }
 
-  int insert(string& str, int idx) {
+  void insert(string& str, int idx) {
+    finishCnt++;
     if(str.size() == idx) {
       finish = true;
-      return 1;
+      return;
     }
+    
     int target = str[idx] - 'a';
+
     if(arr[target] == NULL) {
+      childrenCnt++;
       arr[target] = new Trie();
     }
-    return childrenCnt += arr[target]->insert(str, idx + 1);
+
+    arr[target]->insert(str, idx + 1);
   }
 
   int getClickCnt(string& str, int idx) {
-    cout << childrenCnt << ' ' << str[idx] << "  ||  ";
-    if(childrenCnt <= 1) return 1;
-    else return 1 + arr[str[idx]-'a']->getClickCnt(str, idx + 1);
+    // cout << str[idx] << ' ' << finishCnt << ' ' << childrenCnt << "  ||  ";
+    if(str.size() == idx) return 0;
+    if(finishCnt <= 1) {
+      if(idx == 0) return 1;
+      return 0;
+    }
+
+    int target = str[idx] - 'a';
+
+    if(childrenCnt <= 1 && !finish && 0 < idx) 
+      return arr[target]->getClickCnt(str, idx + 1);
+    else
+      return 1 + arr[target]->getClickCnt(str, idx + 1);
   }
 };
 
@@ -47,6 +63,10 @@ void input() {
 }
 
 int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
   while(cin >> n) {
     input();
     Trie* trie = new Trie();
@@ -57,13 +77,19 @@ int main() {
 
     double cnt = 0;
     for(int i = 0; i < n; i++) {
+      // cout << arr[i] << " : ";
       int res = trie->getClickCnt(arr[i], 0);
-      cout << res << '\n';
+      // cout << res << '\n';
       cnt += (double)(res);
     }
 
-    printf("%.2lf\n", cnt / (double)n);
+    double ans = cnt / (double)n * 100.0;
+    ans = round(ans);
+    ans /= 100.0;
+
+    printf("%.2lf\n", ans);
 
     delete trie;
+    arr.clear();
   }
 }
