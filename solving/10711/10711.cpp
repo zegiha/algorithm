542ed601a5b3213a7b4a7aacbff086ff6f9ce7ec
zegiha <leeseoyul070814@gmail.com>
2025-09-18 09:37:12 +0900

    solve 10711

--- solving/10711/10711.cpp
+++ solving/10711/10711.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 using pii = pair <int, int>;
 
 const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1}, dx[] = {1, -1, 0, 0, -1, 1, 1, -1};
 
-int n, m, arr[1001][1001], newArr[1001][1001];
+int n, m, arr[1001][1001], ans;
 
 bool isArea(int r, int c) {
   return 0 <= r && r < n && 0 <= c && c < m;
 }
 
-bool isBreak(int r, int c) {
-  int cnt = 0;
+bool isBreakNext(int r, int c, int (&arr)[1001][1001]) {
+  int cnt = 0, nr, nc;
   for(int d = 0; d < 8; d++) {
-    if(arr[r + dy[d]][c + dx[d]] <= 0) {
-      cnt++;
-    }
+    nr = r + dy[d], nc = c + dx[d];
+     if(isArea(nr, nc) && arr[nr][nc] == 0) cnt++;
   }
 
   return arr[r][c] <= cnt;
-}
-
-bool isBreakNext(int r, int c) {
-  int cnt = 0;
-  for(int d = 0; d < 8; d++) {
-    if(newArr[r + dy[d]][c + dx[d]] <= 0) {
-      cnt++;
-    }
-  }
-
-  return newArr[r][c] <= cnt;
 }
 
 int main() {
@@ -39,68 +29,56 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
-  queue <pii> q;
-
   cin >> n >> m;
-
   for(int i = 0; i < n; i++) {
     string t; cin >> t;
     for(int j = 0; j < m; j++) {
-      if(t[j] == '.') {
-        arr[i][j] = newArr[i][j] = 0;
-      }
-      else arr[i][j] = newArr[i][j] = t[j] - '0';
+      if(t[j] == '.') arr[i][j] = 0;
+      else arr[i][j] = t[j] - '0';
     }
   }
 
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      if(isBreak(i, j)) {
-        q.push({i, j});
-        newArr[i][j] = 0;
-      }
+  queue <pii> q;
+  vector <tuple<int, int, int>> changed;
+  bool visited[1001][1001] = {0, };
+
+  for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
+    if(isBreakNext(i, j, arr)) {
+      q.push({i, j});
+      changed.push_back({i, j, 0});
+      visited[i][j] = true;
     }
   }
 
-  int ans = 0;
-  bool visited[1001][1001];
   while(!q.empty()) {
-    int size = q.size();
+
+    int size = q.size(), nr, nc;
     ans++;
 
-    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
-      visited[i][j] = false;
-      arr[i][j] = newArr[i][j];
+    for(tuple <int, int, int> v : changed) {
+      auto [i, j, newV] = v;
+      arr[i][j] = newV;
     }
-    // cout << "========================\n";
+    changed.clear();
 
     while(size--) {
       auto [r, c] = q.front();
-      // cout << r << ' ' << c << '\n';
       q.pop();
 
       for(int d = 0; d < 8; d++) {
-        int nr = r + dy[d], nc = c + dx[d];
-
-        if(isArea(nr, nc) && !visited[nr][nc] && arr[nr][nc] > 0) {
+        nr = r + dy[d], nc = c + dx[d];
+        if(
+          isArea(nr, nc) &&
+          arr[nr][nc] != 0 &&
+          isBreakNext(nr, nc, arr) &&
+          !visited[nr][nc]
+        ) {
           visited[nr][nc] = true;
-          if(isBreak(nr, nc)) {
-            q.push({nr, nc});
-            newArr[nr][nc] = 0;
-          }
+          q.push({nr, nc});
+          changed.push_back({nr, nc, 0});
         }
       }
     }
-    // cout << "========================\n";
-
-    // cout << "========================\n";
-    // for(int i = 0; i < n; i++) {
-    //   for(int j = 0; j < m; j++) {
-    //     cout << arr[i][j] << ' ';
-    //   }
-    //   cout << '\n';
-    // }
-    // cout << "========================\n";
   }
 
   cout << ans;
