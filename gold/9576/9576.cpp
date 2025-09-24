b3e1f06dad8de6b2eb834ced566cff54ebae9aa1
zegiha <leeseoyul070814@gmail.com>
2025-09-24 09:52:17 +0900

    solve 9576

--- gold/9576/9576.cpp
+++ gold/9576/9576.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 vector <vector <int>> arr;
 
-bool occupy[1011], visited[1011];
+bool visited[1011], fixedNode[1011];
+int occupy[1011];
 bool isOccupyAndOccupy(int node) {
+  // cout << "node: " << node << '\n';
+
   if(visited[node]) return false;
   visited[node] = true;
 
   for(int i = 0; i < arr[node].size(); i++) {
     int t = arr[node][i];
+
+    // cout << "child: " << t << '\n';
 
     if(occupy[t] == 0 || isOccupyAndOccupy(occupy[t])) {
       occupy[t] = node;
@@ -37,10 +42,20 @@ void solve() {
     }
   }
 
+  // for(int i = 1; i <= m; i++) {
+  //   cout << i << ": ";
+  //   for(auto v : arr[i]) cout << v << ' ';
+  //   cout << '\n';
+  // }
+
   for(int i = 1; i <= m; i++) {
     for(int i = 0; i < 1001; i++) visited[i] = false;
+    // cout << "============================\n";
     if(isOccupyAndOccupy(i)) ans++;
+    // cout << "============================\n";
+    // for(int i = 1; i <= n; i++) cout << occupy[i] << ' '; cout << '\n';
   }
+
 
 
   cout << ans << '\n';
