cff7117db61984b66b88d60287aca1ff8c1ef275
zegiha <leeseoyul070814@gmail.com>
2025-10-10 10:24:45 +0900

    solving 20667

--- solving/20667/20667.cpp
+++ solving/20667/20667.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
-#include <set>
+
+#define PRIORITY_MAX 500
+#define CPU_MAX 1000
+#define INF 2'100'000'000
 
 using namespace std;
 
-struct Item {
-  int w, v1, v2;
-};
-
-int N, W, K;
-Item arr[101];
-set <pair <int, int>> dp[501];
+int priority[101], cpu[101], memory[101], N, M, K, dp[501][1002];
 
 int main() {
-  cin >> N >> W >> K;
-  for(int i = 0; i < N; i++) cin >> arr[i].v1 >> arr[i].v2 >> arr[i].w;
+  cin >> N >> M >> K;
+  for(int i = 0; i < N; i++) cin >> cpu[i] >> memory[i] >> priority[i];
 
-  dp[0].insert({0, 0});
-  for(auto& it : arr) {
-    for(int w = W; w >= it.w; w--) {
-      for(pair <int, int> t : dp[w - it.w]) {
-        int v1 = t.first, v2 = t.second;
-        dp[w].insert({v1 + it.v1, v2 + it.v2});
+  for(int i = 0; i < 501; i++) for(int j = 0; j < 1002; j++) dp[i][j] = -INF;
+  dp[0][0] = 0;
+
+
+  for(int idx = 0; idx < N; idx++) {
+    for(int i = PRIORITY_MAX; i >= 0; i--) {
+      if(i + priority[idx] > PRIORITY_MAX) continue;
+      for(int j = CPU_MAX + 1; j >= 0; j--) {
+        if(dp[i][j] == -INF) continue;
+        dp[i + priority[idx]][min(j + cpu[idx], CPU_MAX + 1)] = max(dp[i + priority[idx]][min(j + cpu[idx], CPU_MAX + 1)], dp[i][j] + memory[idx]);
       }
     }
   }
+
+  int ans = INF;
+  for(int i = PRIORITY_MAX; i >= 0; i--) {
+    for(int j = CPU_MAX; j >= 0; j--) {
+      if(j >= M && dp[i][j] >= K && i < ans) {
+        ans = i;
+      }
+
+      // cout << dp[i][j] << "\n";
+    }
+  }
+  
+  if(ans == INF) cout << -1;
+  else cout << ans;
 }
