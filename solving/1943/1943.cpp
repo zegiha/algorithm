30905fbfe06e1de4c77e1e55c45648d10b7cff15
zegiha <leeseoyul070814@gmail.com>
2025-10-15 12:51:32 +0900

    solving 1943

--- solving/1943/1943.cpp
+++ solving/1943/1943.cpp
@@ -1,46 +1,52 @@
 #include <iostream>
 #include <vector>
 
-#define TOTAL_MAX 100000
-
 using namespace std;
+using pii = pair <int, int>;
 
-int getComp(int total, int v) {
-  return abs(total/2 - v);
-}
+int memo[100][100'001], n;
+pii arr[100];
 
-void solve(int tmp) {
-  int n = tmp, total = 0, dp[100'001] = {0,};
-  vector<int> arr;
+int dp(int idx, int price) {
+  if(price < 0) return 0;
+  if(idx == n) return price == 0;
 
-  for(int i = 0; i < n; i++) {
-    int a, b; cin >> a >> b;
-    while(b--) {
-      arr.push_back(a);
-      total += a;
-    }
+  if(memo[idx][price] != -1) return memo[idx][price];
+
+  int res = 0;
+  for(int i = 0; i <= arr[idx].second; i++) {
+    res = max(res, dp(idx + 1, price - arr[idx].first * i));
   }
 
-  if(total % 2 != 0) {
-    cout << 0 << '\n';
-    return;
-  }
-
-  n = arr.size();
-  dp[0] = 1;
-
-  for(int idx = 0; idx < n; idx++) {
-    for(int i = TOTAL_MAX; i >= 0; i--) {
-      if(dp[i]) dp[i + arr[idx]] = 1;
-    }
-  }
-
-  cout << dp[total / 2] << '\n';
+  return memo[idx][price] = res;
 }
 
 int main() {
-  int n;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
   while(cin >> n) {
-    solve(n);
+    int total = 0;
+    for(int i = 0; i < n; i++) {
+      cin >> arr[i].first >> arr[i].second;
+      total += arr[i].first * arr[i].second;
+    }
+    if(total % 2 != 0) cout << 0 << '\n';
+
+    for(int i = 0; i < n; i++) for(int j = 0; j <= total; j++) memo[i][j] = -1;
+    dp(0, total / 2);
+    bool sw = false;
+    for(int i = 0; i < n; i++) {
+      if(!sw && memo[i][total / 2] == 1) {
+        cout << 1 << '\n';
+        sw = true;
+        break;
+      }
+    }
+    if(!sw) cout << 0 << '\n';
+    // cout << "=========================\n\n";
+    // for(int i = 0; i < n; i++) for(int j = 0; j <= total; j++) if(memo[i][j] != -1) cout << i << ", " << j << " : " << memo[i][j] << '\n';
+    // cout << "=========================\n\n";
   }
 }
